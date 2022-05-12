6c447dcf89bb1481a1c1caa484e2b49e674d8149
Irving Delgado <rvngdelgado@gmail.com>
2022-05-12 12:55:11 -0500

    fixed errors

--- api/server.js
+++ api/server.js
@@ -1,14 +1,43 @@
 const express = require('express')
 const fortunes = require("./fortunes")
+const Stuff = require("./stuff-model")
 
 const server = express()
+
+server.use(express.json())
 
 server.get('/', (req,res) => {
     res.send(`<h1>Heroku Deployment</h1>`)
 })
 
 server.get("/fortune", (req,res) => {
-    let randonIndex = Math.floor(Math.random() * fortunes.length)
+    let randomIndex = Math.floor(Math.random() * fortunes.length);
+    res.send(`<h1>${fortunes[randomIndex]}</h1>`)
+})
+
+server.get('/stuff', (req,res) => {
+    Stuff.getAll()
+    .then(result => {
+        res.json(result)
+    })
+})
+
+server.get('/stuff/:index', (req,res) => {
+    Stuff.getByIndex(req.params.index)
+    .then(result => {
+        if(result == null) {
+            res.status(404).json({message: 'not found'})
+            return;
+        }
+        res.json(result)
+    })
+})
+
+server.post('/stuff', (req,res) => {
+    Stuff.add(req.body)
+    .then(result => {
+        res.status(201).json(result)
+    })
 })
 
 module.exports = server            
